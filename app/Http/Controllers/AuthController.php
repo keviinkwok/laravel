187ee6168dbaa2cbd763df0c91f532ff3d7f52dc
Kevin <keviin.kwok@gmail.com>
2020-08-04 18:39:48 +0700

    Tugas 12

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function index(){
+        return view('register');
+    }
+
+    public function welcome(Request $request){
+        $fname = $request->input('fname');
+        $lname = $request->input('lname');
+        
+        return view('welcome',compact('fname','lname'));
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+
+    <h1>Selamat Datang {{ $fname }} {{ $lname }}</h1>
+    <h3>Terimakasih telah bergabung di SanberBook. Social Media kita bersama!</h3>
+    
+</body>
+</html>
